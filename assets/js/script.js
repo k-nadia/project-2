84b2965d3189fe42889c245aed700f2dbf86bfec
Katia Laws <knadia@live.com>
2024-08-07 10:31:33 +0000

    Declare startTimer function which will countdown from 1 minute in 1 second increments

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,9 +11,22 @@ function loadRandomExcerpt() {
     });
 }
 
+function startTimer(duration, display) {
+    let timer = duration, minutes, seconds;
+    const interval = setInterval(function () {
+        minutes = parseInt(timer / 60, 10);
+        seconds = parseInt(timer % 60, 10);
 
-function startTimer() {
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
 
+        display.textContent = minutes + ":" + seconds;
+
+        if (--timer < 0) {
+            clearInterval(interval);
+            display.textContent = "00:00";
+        }
+    }, 1000);
 }
 
 function checkTyping() {
